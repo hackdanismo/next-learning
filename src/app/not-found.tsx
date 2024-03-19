31ba7cf02855aa2a4a4d9840406482ff82805f87
hackdanismo <office@justtoconfirm.co.uk>
2024-03-19 13:59:09 +0000

    feat: metadata for the page title and description added to each page template using the static metadata object built-in to Next

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next"
+
+// This is the static metadata object to add metadata to the page
+export const metadata: Metadata = {
+  title: "Next App: 404 Not Found",
+  description: "This is the 404 Not Found page for the Next App."
+}
+
 const NotFound = () => {
   return (
     <div>404: Not Found</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import type { Metadata } from "next"
+
 import Heading from "@/components/heading"
 import Button from "@/components/button"
 import AppLink from "@/components/link"
+
+// This is the static metadata object to add metadata to the page
+export const metadata: Metadata = {
+  title: "Next App: Home",
+  description: "This is the Home page for the Next App."
+}
 
 const Home = () => {
   return (
